b2be840c89ec4f48384476122e8f78f2a1073b87
Guilherme Lima <guilhermelucas.contato@gmail.com>
2024-02-07 12:43:10 -0300

    Creating producer and consumer

---
+++ src/Vehicle.Consumer/Program.cs
@@ -0,0 +1,17 @@
+﻿using Confluent.Kafka;
+
+ConsumerConfig config = new()
+{
+    GroupId = "Vehicle.Dev.IO",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe("vehicle-dev");
+
+while (true)
+{
+    var result = consumer.Consume();
+    Console.WriteLine($"Message: {result.Message.Key}-{result.Message.Value}");
+}
---
+++ src/Vehicle.Producer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Confluent.Kafka;
+
+ProducerConfig config = new()
+{
+    BootstrapServers = "localhost:9092"
+};
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+var message = new Message<string, string>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = $"Message test plate: LSN4444 date: {DateTime.UtcNow}"
+};
+
+var result = await producer.ProduceAsync("vehicle-dev", message);
+
+Console.WriteLine($"Offset: {result.Offset}, Partition: {result.Partition}");
